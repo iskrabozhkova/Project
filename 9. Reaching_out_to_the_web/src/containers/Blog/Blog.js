617244508cf350ab7da7677e7b401ebef118fa83
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2021-07-25 21:46:23 +0300

    exercise chapter 9

---
+++ 9. Reaching_out_to_the_web/src/containers/Blog/Blog.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios'
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response =>{
+            const posts = response.data.slice(0,4);
+            const updatePosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Iskra'
+                }
+            })
+           this.setState({posts: updatePosts});
+        })
+    }
+    render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title = {post.title} author={post.author}/>
+        })
+        return (
+            <div>
+                <section className="Posts">
+                   {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
