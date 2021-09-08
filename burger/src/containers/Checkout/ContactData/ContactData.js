a4c9a3043c3e676e56836166d01a37aabffb6b90
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2021-09-08 08:43:46 +0300

    commit

--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -1,27 +1,106 @@
 import React, {Component} from 'react'
 import Button from '../../../components/UI/Button/Button'
 import './ContactData.css'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component{
     state = {
-        name: '',
-        email: '',
-        address: ''
+        // name: '',
+        // email: '',
+        // address: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your name'
+                },
+                value:'',
+               valid: false,
+               touched: false
+            },
+            address: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your address'
+                },
+                value: '',
+                valid: false,
+                touched: false
+            },
+            email: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Your email'
+                    },
+                    value:'',
+                    valid: false,
+                    touched: false
+            },
+            deliveryMethod: {
+                    elementType: 'select',
+                    elementConfig: {
+                        options: [
+                            {value: 'fastest', displayValue: 'Fastest'},
+                            {value: 'cheapest', displayValue: 'Cheapest'}
+                        ]
+                    },
+                   value: ''
+            }
+        }
     }
 
+    checkValidity(value){
+        let isValid = false;
+        if(value.trim() !== ''){
+            isValid = true;
+        }
+        return isValid;
+    }
     orderHandler(event){
         event.preventDefault();
         console.log(this.props.ingredients);
     }
+    inputChangeHandler = (event, inputId) => {
+        const updatedOrderForm ={
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputId]
+        }
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid=this.checkValidity(updatedFormElement.value);
+        updatedOrderForm[inputId]=updatedFormElement;
+        updatedFormElement.touched = true;
+        console.log(updatedFormElement);
+        this.setState({orderForm: updatedOrderForm});
+    }
 
     render(){
+        const formElementsArray=[];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         return (
             <div className="ContactData">
                 <h4>Enter your contact data: </h4>
                 <form>
-                    <input className="Input" type="text" name="name" placeholder="Your name"/>
-                    <input className="Input" type="text" name="email" placeholder="Your email"/>
-                    <input  className="Input" type="text" name="address" placeholder="Your addres"/>
+                    {formElementsArray.map(formElement => (
+                        <Input 
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            invalid={!formElement.config.valid}
+                            touched={formElement.touched}
+                            changed={(event) => this.inputChangeHandler(event, formElement.id)}
+                        />
+                    ))}
                     <Button clicked={this.orderHandler}>ORDER</Button>
                 </form>
             </div>
---
+++ burger/src/containers/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Order.css'
+
+const order = (props) => {
+    <div>
+        <p>Ingredients: </p>
+        <p>Price: </p>
+    </div>
+}
+
+export default order;
