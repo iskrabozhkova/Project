8ae91e40ca37a698bb9121fb51002318006ef08a
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2021-09-08 20:09:26 +0300

    commit

---
+++ redux--01-start/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import reducer from './store/reducer'
+
+const store=createStore();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
